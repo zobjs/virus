a8f4dd46a9ad5dfb7b7208a968033e9ef59f95d5
zobjs <experiment.zobaidulkazi@gmail.com>
2024-08-10 03:10:27 +0600

    Update index.js876

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const config = {
-  PORT: process.env.PORT || 5001,
+  PORT: process.env.PORT || 7654,
   MONGO_URI: process.env.MONGO_URI || "",
   JWT_SECRET: process.env.JWT_SECRET,
 };
