adef48f61050b0f28f061b1666353a67db7e6a9b
zobjs <experiment.zobaidulkazi@gmail.com>
2024-08-10 05:27:46 +0600

    Update index.jsss

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,11 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const  MONGO_URI = "mongodb+srv://exzobaidulkazi:tapMTRazUYw4mod6@cluster0.f143vc0.mongodb.net/kazi01"
+
 const config = {
   PORT: process.env.PORT || 7654,
-  MONGO_URI: process.env.MONGO_URI || "",
+  MONGO_URI:  MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
 };
 
